ce04f93338f8edc4c24fdd3ba7d0041c168559d5
Cong Nguyen <cong.nguyenquoc01@icloud.com>
2024-03-04 13:23:35 +0700

    Setting up Testing Location

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String google_api_key = 'AIzaSyCv46WaFWEk94KBGK0YAxo3t-DGgFQEO7s';
+const primaryColor = Color(0xff7b61ff);
+const defaultPadding = 16;
---
+++ lib/order_tracking_page.dart
@@ -0,0 +1,104 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:live_location_tracking/constants.dart';
+import 'package:location/location.dart';
+
+class OrderTrackingPage extends StatefulWidget {
+  const OrderTrackingPage({super.key});
+
+  @override
+  State<OrderTrackingPage> createState() => _OrderTrackingPageState();
+}
+
+class _OrderTrackingPageState extends State<OrderTrackingPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+  static const LatLng sourceLocation = LatLng(37.33500926, -122.03272188);
+  static const LatLng destination = LatLng(37.33429383, -122.06600055);
+  List<LatLng> polylineCoordinates = [];
+  LocationData? currentLocation;
+  void getCurrentLocation() async {
+    Location location = Location();
+
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    location.getLocation().then(
+      (location) {
+        currentLocation = location;
+        print(currentLocation);
+      },
+    );
+  }
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        google_api_key,
+        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+        PointLatLng(destination.latitude, destination.longitude));
+    if (result.points.isNotEmpty) {
+      result.points.forEach((PointLatLng point) =>
+          polylineCoordinates.add(LatLng(point.latitude, point.longitude)));
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    getCurrentLocation();
+
+    getPolyPoints();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Track Order'),
+      ),
+      body: currentLocation == null
+          ? Center(
+              child: Text('Loading...'),
+            )
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: sourceLocation,
+                zoom: 13.5,
+              ),
+              polylines: {
+                  Polyline(
+                      polylineId: PolylineId('route'),
+                      points: polylineCoordinates,
+                      color: primaryColor,
+                      width: 6),
+                },
+              markers: {
+                  Marker(
+                      markerId: MarkerId('sourceLocation'),
+                      position: sourceLocation),
+                  Marker(
+                      markerId: MarkerId('destination'), position: destination),
+                }),
+    );
+  }
+}
